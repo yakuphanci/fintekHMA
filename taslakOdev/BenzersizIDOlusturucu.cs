98305fd97a3203501eca81eea8fdfd38790af14a
Yakup <61211736+yakuphanci@users.noreply.github.com>
2021-05-13 04:14:02 +0300

    Benzersiz ID oluşturan bir sınıf eklendi.
    PazarID ve urunID(kategori) için kullanacağımız benzersiz ID oluşturan bir sınıf eklendi.

---
+++ taslakOdev/BenzersizIDOlusturucu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taslakOdev
+{
+    public class IDTutucu
+    {
+        public uint pazarID;
+        public uint urunID;
+
+    }
+
+    public static class BenzersizIDOlusturucu
+    {
+        public static uint GetPazarID()
+        {
+            var idTutucu = JsonController.GetDataFromJSON<IDTutucu>(JsonController.GetJsonFromFile(@"benzersizIDler.json"));
+            idTutucu.pazarID++;
+            JsonController.SaveJsonToFile(@"benzersizIDler.json", idTutucu);
+            return idTutucu.pazarID;
+        }
+        public static uint GetUrunID()
+        {
+            var idTutucu = JsonController.GetDataFromJSON<IDTutucu>(JsonController.GetJsonFromFile(@"benzersizIDler.json"));
+            idTutucu.urunID++;
+            JsonController.SaveJsonToFile(@"benzersizIDler.json", idTutucu);
+            return idTutucu.urunID;
+        }
+
+
+    }
+}
